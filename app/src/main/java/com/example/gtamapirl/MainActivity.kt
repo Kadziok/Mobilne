e51fe68cfaad66e175c496b5cd1ff2b3071a3520
Arczi-pl <artursikorski4@gmail.cp>
2021-06-02 22:34:53 +0200

    add firebase

--- app/src/main/java/com/example/gtamapirl/MainActivity.kt
+++ app/src/main/java/com/example/gtamapirl/MainActivity.kt
@@ -1,14 +1,47 @@
 package com.example.gtamapirl
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+
+const val RC_SIGN_IN = 1234
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var database: DatabaseReference
+    private lateinit var cUser: FirebaseUser
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .build(),
+            RC_SIGN_IN
+        )
+
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == Activity.RESULT_OK) {
+                cUser = FirebaseAuth.getInstance().currentUser!!
+            }
+        }
+    }
+
+
+
+
 
     fun getMyLocation(view: View) {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.fragment) as MapFragment
