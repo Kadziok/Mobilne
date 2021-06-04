ec178348813604e020713883017400e55c2f4540
Arczi-pl <artursikorski4@gmail.cp>
2021-06-04 15:32:24 +0200

    add time to event and repair Piotrek's map fragment

--- app/src/main/java/com/example/gtamapirl/AddEventFragment.kt
+++ app/src/main/java/com/example/gtamapirl/AddEventFragment.kt
@@ -1,6 +1,7 @@
 package com.example.gtamapirl
 
 import android.os.Bundle
+import android.text.format.Time
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,13 +11,16 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog
+import com.wdullaer.materialdatetimepicker.time.TimePickerDialog
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
 class AddEventFragment : Fragment() {
 
     private var date: LocalDate? = null
+    private var time: LocalTime? = null
     private var binding: FragmentAddEventBinding? = null
 
     override fun onCreateView(
@@ -33,6 +37,7 @@ class AddEventFragment : Fragment() {
         binding!!.fab.setOnClickListener {
             addEvent()
         }
+
         binding!!.dateInput.setOnClickListener {
             setDate()
         }
@@ -45,6 +50,19 @@ class AddEventFragment : Fragment() {
             binding!!.dateInput.setText("")
         }
 
+
+        binding!!.timeInput.setOnClickListener {
+            setTime()
+        }
+        binding!!.timeInput.setOnFocusChangeListener { _: View, focused: Boolean ->
+            if(focused)
+                setTime()
+        }
+        binding!!.timeInputLayout.setEndIconOnClickListener {
+            date = null
+            binding!!.dateInput.setText("")
+        }
+
     }
 
     private fun setDate() {
@@ -52,31 +70,54 @@ class AddEventFragment : Fragment() {
         val currentYear: Int = now.get(Calendar.YEAR)
         val currentMonth: Int = now.get(Calendar.MONTH)
         val currentDay: Int = now.get(Calendar.DAY_OF_MONTH)
-        val datePickerDialog = DatePickerDialog.newInstance({ _, year, monthOfYear, dayOfMonth ->
+        val dateListener = DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
             date = LocalDate.of(year, monthOfYear, dayOfMonth)
             binding!!.dateInput.setText(date?.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
             binding!!.dateInputLayout.isErrorEnabled = false
-        }, currentYear, currentMonth, currentDay)
+        }
+        val datePickerDialog = DatePickerDialog.newInstance(dateListener, currentYear, currentMonth, currentDay)
         datePickerDialog.setTitle(getString(R.string.addEvent_datePicker_title))
         datePickerDialog.accentColor = resources.getColor(R.color.colorPrimary)
 
         datePickerDialog.show(childFragmentManager, null)
     }
 
+    private fun setTime() {
+        val now = Calendar.getInstance()
+        val currentHour: Int = now.get(Calendar.HOUR_OF_DAY)
+        val currentMinute: Int = now.get(Calendar.MINUTE)
+        val timeListener = TimePickerDialog.OnTimeSetListener { _, hour, minute, _ ->
+            time = LocalTime.of(hour, minute, 0)
+            binding!!.timeInput.setText(time?.format(DateTimeFormatter.ofPattern("HH:mm")))
+            binding!!.timeInputLayout.isErrorEnabled = false
+        }
+        val timePickerDialog = TimePickerDialog.newInstance(timeListener, currentHour, currentMinute, false)
+        timePickerDialog.title = getString(R.string.addEvent_timePicker_title)
+        timePickerDialog.accentColor = resources.getColor(R.color.colorPrimary)
+
+        timePickerDialog.show(childFragmentManager, null)
+    }
+
     private fun addEvent() {
         binding!!.textInputNameLayout.isErrorEnabled = false
         binding!!.dateInputLayout.isErrorEnabled = false
 
-        if(binding!!.textInputName.text!!.isEmpty()) {
-            binding!!.textInputNameLayout.isErrorEnabled = true
-            binding!!.textInputNameLayout.error = getString(R.string.addEvent_error_name_blank)
-        }
-        else if(date==null) {
-            binding!!.dateInputLayout.isErrorEnabled = true
-            binding!!.dateInputLayout.error = getString(R.string.addEvent_error_date_empty)
-        }
-        else {
-            //TODO adding event
+        when {
+            binding!!.textInputName.text!!.isEmpty() -> {
+                binding!!.textInputNameLayout.isErrorEnabled = true
+                binding!!.textInputNameLayout.error = getString(R.string.addEvent_error_name_blank)
+            }
+            date==null -> {
+                binding!!.dateInputLayout.isErrorEnabled = true
+                binding!!.dateInputLayout.error = getString(R.string.addEvent_error_date_empty)
+            }
+            time==null -> {
+                binding!!.timeInputLayout.isErrorEnabled = true
+                binding!!.timeInputLayout.error = getString(R.string.addEvent_error_time_empty)
+            }
+            else -> {
+                //TODO adding event
+            }
         }
     }
 
--- app/src/main/java/com/example/gtamapirl/MapFragment.kt
+++ app/src/main/java/com/example/gtamapirl/MapFragment.kt
@@ -94,20 +94,21 @@ class MapFragment : Fragment() {
         val cameraPosition = CameraPosition.Builder()
                 .target(position)
                 .zoom(zoom)
-                .bearing(tilt)
-                .tilt(bearing)
+                .bearing(bearing)
+                .tilt(tilt)
                 .build()
         map?.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
     }
 
     private fun setLocation() {
         if(locationPermissionGranted && lastLocation!=null) {
-            map?.moveCamera(
-                  CameraUpdateFactory.newLatLngZoom(
-                        LatLng(lastLocation!!.latitude, lastLocation!!.longitude),
-                        DEFAULT_ZOOM.toFloat()
-                  )
-            )
+            val cameraPosition = CameraPosition.Builder()
+                .target( LatLng(lastLocation!!.latitude, lastLocation!!.longitude))
+                .zoom(DEFAULT_ZOOM.toFloat())
+                .bearing(0.0f)
+                .tilt(0.0f)
+                .build()
+            map?.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
         }
         else {
             getLocationPermission()
