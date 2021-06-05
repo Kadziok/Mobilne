86206f0135d9aeef086efb27c6a95c2d1aeb7251
Arczi-pl <artursikorski4@gmail.cp>
2021-06-05 23:53:18 +0200

    add time to chat msgs

--- app/src/main/java/com/example/gtamapirl/data/MessageData.kt
+++ app/src/main/java/com/example/gtamapirl/data/MessageData.kt
@@ -1,13 +1,15 @@
 package com.example.gtamapirl.data
 
-class Message {
+class MessageData {
     var text: String? = null
     var userid: String? = null
+    var timeStamp: Long? = null
 
     constructor()
 
-    constructor(text: String?, id: String?) {
+    constructor(text: String?, id: String?, timeStamp: Long?) {
         this.text = text
         this.userid = id
+        this.timeStamp = timeStamp
     }
 }
--- app/src/main/java/com/example/gtamapirl/ui/chat/MessageAdapter.kt
+++ app/src/main/java/com/example/gtamapirl/ui/chat/MessageAdapter.kt
@@ -1,19 +1,25 @@
 package com.example.gtamapirl.ui.chat
 
 import android.graphics.Color
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.gtamapirl.R
-import com.example.gtamapirl.data.Message
+import com.example.gtamapirl.data.MessageData
 import com.example.gtamapirl.databinding.ItemMessageBinding
 import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.firebase.ui.database.FirebaseRecyclerOptions
+import java.sql.Date
+import java.sql.Time
+import java.text.SimpleDateFormat
+import java.util.*
 
-class MessageAdapter(options: FirebaseRecyclerOptions<Message>,
+class MessageAdapter(options: FirebaseRecyclerOptions<MessageData>,
                      val fragment: ChatFragment, val myId: String):
-    FirebaseRecyclerAdapter<Message, RecyclerView.ViewHolder>(options) {
+    FirebaseRecyclerAdapter<MessageData, RecyclerView.ViewHolder>(options) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -22,15 +28,33 @@ class MessageAdapter(options: FirebaseRecyclerOptions<Message>,
         return MessageHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, model: Message) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, model: MessageData) {
         (holder as MessageAdapter.MessageHolder).bind(model)
     }
 
+
+
     inner class MessageHolder(private val binding: ItemMessageBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: Message) {
+        fun bind(item: MessageData) {
             binding.messageTextView.text = item.text
             setTextColor(item.userid, binding.messageTextView)
+            if (item.timeStamp != null) {
+                val timestamp = Date(item.timeStamp!!)
+                val dateString = timestamp.dateToString("E: hh:mm")
+                binding.timestamp.text = dateString.toString()
+            } else {
+                binding.timestamp.text = "Before timestamp future"
+            }
+
+            binding.messageLay.setOnClickListener {
+                if (binding.timestamp.visibility == View.GONE){
+                    binding.timestamp.visibility = View.VISIBLE
+                } else {
+                    binding.timestamp.visibility = View.GONE
+                }
+
+            }
         }
 
         private fun setTextColor(userId: String?, textView: TextView) {
@@ -44,4 +68,9 @@ class MessageAdapter(options: FirebaseRecyclerOptions<Message>,
         }
     }
 
+}
+
+private fun Date.dateToString(s: String): Any {
+    val dateFormatter = SimpleDateFormat(s, Locale.getDefault())
+    return dateFormatter.format(this)
 }
